51e4e87cf82ea2dbc823432566fb4f74ea8e37e0
Krimothiazine <krimouv1995@gmail.com>
2023-07-09 17:23:42 +0100

    Add color to timer

--- src/components/header/Timer.jsx
+++ src/components/header/Timer.jsx
@@ -3,10 +3,17 @@ import styled from 'styled-components';
 const Container = styled.div`
   font-family: 'Bangers', cursive;
   letter-spacing: 5px;
+  & span {
+    color: red;
+  }
 `;
 
 const Timer = ({ timeFormat }) => {
-  return <Container>Time: {timeFormat}</Container>;
+  return (
+    <Container>
+      Time: <span>{timeFormat}</span>
+    </Container>
+  );
 };
 
 export default Timer;
